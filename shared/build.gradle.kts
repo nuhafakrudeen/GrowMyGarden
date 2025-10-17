353a8247b058518f7e86908db4fc45442d457533
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2025-10-17 10:52:11 -0700

    Update CI

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+//    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
@@ -29,17 +29,19 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
+
     }
+
 }
 
-android {
-    namespace = "com.gmg.growmygarden.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-}
+//android {
+//    namespace = "com.gmg.growmygarden.shared"
+//    compileSdk = libs.versions.android.compileSdk.get().toInt()
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_11
+//        targetCompatibility = JavaVersion.VERSION_11
+//    }
+//    defaultConfig {
+//        minSdk = libs.versions.android.minSdk.get().toInt()
+//    }
+//}
