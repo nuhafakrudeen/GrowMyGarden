8a329268860f66133d89aeab52daf0e9c910c509
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2025-11-06 17:54:03 -0800

    Fix LoginViewModel

--- shared/src/commonMain/kotlin/com/gmg/growmygarden/viewmodel/LoginViewModel.kt
+++ shared/src/commonMain/kotlin/com/gmg/growmygarden/viewmodel/LoginViewModel.kt
@@ -1,10 +1,24 @@
 package com.gmg.growmygarden.viewmodel
 
+import com.gmg.growmygarden.auth.LoginService
+import com.gmg.growmygarden.auth.LoginStatus
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import com.rickclephas.kmp.observableviewmodel.ViewModel
+import com.rickclephas.kmp.observableviewmodel.stateIn
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.parameter.parametersOf
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel : ViewModel(), KoinComponent {
+
+    private val loginService: LoginService by inject { parametersOf(viewModelScope) }
+
     @NativeCoroutinesState
-    val loginState: StateFlow<LoginStatus>
+    val loginState: StateFlow<LoginStatus> = loginService.loginStatus.stateIn(
+        viewModelScope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000L),
+        initialValue = LoginStatus.UNINITIALIZED,
+    )
 }
