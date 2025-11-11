9eb459db4e67ca5e6778619f89363b56963c3f99
Nuha <nfakrude@ucsc.edu>
2025-11-10 20:47:59 -0800

    new updated code with auth

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,13 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct iOSApp: App {
     @StateObject private var auth = AuthManager()
+    
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
