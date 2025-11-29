0fcb1a2b929cefd40b52eb49d23d6b9ed9adf1d9
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2025-11-29 13:46:49 -0800

    Add better errors

--- shared/src/iosMain/kotlin/com/gmg/growmygarden/di/Helper.ios.kt
+++ shared/src/iosMain/kotlin/com/gmg/growmygarden/di/Helper.ios.kt
@@ -8,16 +8,18 @@ import platform.Foundation.stringWithContentsOfFile
 actual fun getPropertiesMap(): Map<String, Any> {
     val fileName = "koin"
     val type = "properties"
-    return (
+    val path =
         NSBundle.mainBundle.pathForResource(fileName, ofType = type) ?: NSBundle.allBundles.map { it as NSBundle }
             .firstNotNullOfOrNull { it.pathForResource(fileName, ofType = type) }
-        )?.let { path ->
-        NSString.stringWithContentsOfFile(path, encoding = NSUTF8StringEncoding, error = null)?.lines()?.map {
-            it.split("=", limit = 2)
-        }?.filter {
-            it.size == 2
-        }?.associate { (key, value) ->
-            key.trim() to value.trim()
-        }
-    } ?: emptyMap()
+            ?: throw Exception("Failed to find Properties Path")
+
+    val contents = NSString.stringWithContentsOfFile(path, encoding = NSUTF8StringEncoding, error = null)
+        ?: throw Exception("Failed to Load Properties File")
+    return contents.lines().map {
+        it.split("=", limit = 2)
+    }.filter {
+        it.size == 2
+    }.associate { (key, value) ->
+        key.trim() to value.trim()
+    }
 }
