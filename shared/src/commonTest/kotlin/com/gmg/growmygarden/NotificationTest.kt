cfc904ef7b6f40863913b4101a66159b75cec784
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2025-11-28 20:45:28 -0800

    Fix Notification Test Construction

--- shared/src/commonTest/kotlin/com/gmg/growmygarden/NotificationTest.kt
+++ shared/src/commonTest/kotlin/com/gmg/growmygarden/NotificationTest.kt
@@ -1,17 +1,27 @@
 package com.gmg.growmygarden
 
+import com.gmg.growmygarden.di.initKoin
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.Month
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
 import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 
-class NotificationTest(
-    private val notificationHandler: NotificationHandler,
-) {
+class NotificationTest : KoinTest {
+    private val notificationHandler: NotificationHandler by inject<NotificationHandler>()
+
+    @BeforeTest
+    fun startKoin() {
+        initKoin()
+    }
 
     @AfterTest
     fun cleanup() {
         notificationHandler.cancelNotification("67")
+        stopKoin()
     }
 
     @Test
