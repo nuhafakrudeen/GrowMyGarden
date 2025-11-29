9fc6cb4f9b6845530846d63030b61754f94c69b9
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2025-11-28 19:33:08 -0800

    Add Intantiation Test for API
    test koin properties by trying to instantiate an API client

---
+++ shared/src/commonTest/kotlin/com/gmg/growmygarden/APIKoinTest.kt
@@ -0,0 +1,23 @@
+package com.gmg.growmygarden
+
+import com.gmg.growmygarden.di.initKoin
+import com.gmg.growmygarden.network.PerenualApi
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class APIKoinTest : KoinTest {
+
+    @BeforeTest
+    fun setupKoin() {
+        initKoin()
+    }
+    val api: PerenualApi by inject<PerenualApi>()
+
+    @Test
+    fun instantiationTest() {
+        assertNotNull(api)
+    }
+}
