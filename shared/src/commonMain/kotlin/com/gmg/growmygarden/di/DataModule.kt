2fb688b2c696a5e8749c62eeb8a968727b8adf78
cfang15 <cfang15@ucsc.edu>
2025-12-02 12:54:17 -0800

    Fixed crashes from recent duration change

--- shared/src/commonMain/kotlin/com/gmg/growmygarden/di/DataModule.kt
+++ shared/src/commonMain/kotlin/com/gmg/growmygarden/di/DataModule.kt
@@ -1,13 +1,28 @@
-package di
+package com.gmg.growmygarden.di
 
 import com.gmg.growmygarden.data.db.DatabaseProvider
+import com.gmg.growmygarden.data.source.PlantImageStore
 import com.gmg.growmygarden.data.source.PlantInfoRepository
 import com.gmg.growmygarden.data.source.PlantRepository
+import com.gmg.growmygarden.data.image.PlantScopeProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = module {
-    single { DatabaseProvider() }
+    single<CoroutineDispatcher> { Dispatchers.Default }
+    single<CoroutineContext> { get<CoroutineDispatcher>() }
+    single<CoroutineScope> { CoroutineScope(get<CoroutineContext>() + SupervisorJob()) }
+    singleOf(::DatabaseProvider)
+
     singleOf(::PlantRepository)
     singleOf(::PlantInfoRepository)
+
+    singleOf(::PlantScopeProvider)
+    singleOf(::PlantImageStore)
 }
