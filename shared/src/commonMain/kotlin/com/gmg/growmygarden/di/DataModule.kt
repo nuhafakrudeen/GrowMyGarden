bee394e90374b3bf16c67d7382ccf4fb96f0bf34
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2025-12-02 13:18:59 -0800

    Lint

--- shared/src/commonMain/kotlin/com/gmg/growmygarden/di/DataModule.kt
+++ shared/src/commonMain/kotlin/com/gmg/growmygarden/di/DataModule.kt
@@ -1,18 +1,17 @@
 package com.gmg.growmygarden.di
 
 import com.gmg.growmygarden.data.db.DatabaseProvider
+import com.gmg.growmygarden.data.image.PlantScopeProvider
 import com.gmg.growmygarden.data.source.PlantImageStore
 import com.gmg.growmygarden.data.source.PlantInfoRepository
 import com.gmg.growmygarden.data.source.PlantRepository
-import com.gmg.growmygarden.data.image.PlantScopeProvider
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlin.coroutines.CoroutineContext
 import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
+import kotlin.coroutines.CoroutineContext
 
 val dataModule = module {
     single<CoroutineDispatcher> { Dispatchers.Default }
